44eed82292bbf9dc195d38c3d7e834b66995af56
jiangtao <jiangtao.cctv@qq.com>
2024-12-12 15:32:43 +0800

    mp2vue： 可以显示了

--- mp2vue/src/main/java/io/github/mxvc/mp2vue/Main.java
+++ mp2vue/src/main/java/io/github/mxvc/mp2vue/Main.java
@@ -2,12 +2,13 @@ package io.github.mxvc.mp2vue;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.StrUtil;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
 
-import java.io.File;
 import java.io.IOException;
 
 public class Main {
@@ -15,32 +16,69 @@ public class Main {
     public static final String TARGET = "D:\\ws\\yunying-sichuan-app\\pages\\points\\mall\\mall.vue";
 
 
-
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.parse(ResourceUtil.readUtf8Str("source.html"));
+        Document doc = Jsoup.parse(ResourceUtil.readUtf8Str("source.html"), Parser.xmlParser());
 
-        Element view = doc.selectFirst("body");
+        Element body = doc.selectFirst("body");
+
+        Elements elements = body.select("*");
+        for (Element element : elements) {
+            String name = element.tagName();
+            System.out.println(name);
+            if (name.startsWith("wx-mp-html")) {
+                element.remove();
+                continue;
+            }
+            if (name.equals("span")) {
+                String text = element.text();
+                if (StrUtil.isBlank(text)) {
+                    element.remove();
+                    continue;
+                }
+            }
+
+            if (name.startsWith("wx-")) {
+                element.tagName(name.substring(3));
+            }
+
+            if (name.equals("wx-image")) {
+                element.empty();
+            }
+            if (name.equals("wx-text")) {
+                element.html(element.text());
+            }
+
+            if(element.hasAttr("is")){ // is 有些@符号会报错
+                element.removeAttr("is");
+            }
+
+        }
 
 
-        System.out.println(view);
-
-        replaceTargetTemplate(view.html());
+        replaceTargetTemplate(body.firstElementChild());
 
     }
 
 
-    private static void replaceTargetTemplate(String content){
+    private static void replaceTargetTemplate(Element view) {
         String vue = FileUtil.readUtf8String(TARGET);
 
-        vue = "<html>" + vue + "</html>";
+        vue = "<root>" + vue + "</root>";
 
-        Document doc = Jsoup.parse(vue);
+        Document root = Jsoup.parse(vue, Parser.xmlParser());
 
-        Element t = doc.selectFirst("template");
-        t.html(content);
+        Element t = root.selectFirst("template");
+        t.empty();
+        t.appendChild(view);
 
 
-        FileUtil.writeUtf8String(TARGET, t.html());
+        String result = root.html();
+
+        result = result.replace("<root>", "").replace("</root>", "");
+
+
+        System.out.println(result);
+        FileUtil.writeUtf8String(result, TARGET);
     }
 
 }
